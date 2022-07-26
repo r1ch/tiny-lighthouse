de9a19913f92e4bd668834a41e7236b1190d9d65
r1ch <rich@bradi.sh>
2022-07-26 08:58:17 +0100

    Many comments for clarity
    Also removed repeated declaration from the loop

--- main.py
+++ main.py
@@ -37,7 +37,8 @@ class WrappedRTC:
     def linear(self):
         [hour,minute] = self.datetime()
         return hour*60 + minute
-    
+      
+#a space in 7x1 leds (all off)    
 s = [
             [0],
             [0],
@@ -48,6 +49,7 @@ s = [
             [0]
         ]
 
+#numbers : 0 to 9 in 7x3 leds  
 n = [
             [
                 [0,1,0],
@@ -136,21 +138,27 @@ n = [
 mode = 0
 sleep = 0.5
 
-greenTime = Time(7,0)
+#colours in rgb for the lighthouse
 green = [0,255,0]
-
-redTime = Time(19,0)
 red = [245,40,20]
-
-rtcTime = WrappedRTC()
 white = [255,255,255]
 
+#as standard, turn green at 0700h
+greenTime = Time(7,0)
+
+#as standard, turn red at 1900h
+redTime = Time(19,0)
+
+
+rtcTime = WrappedRTC()
+
+
+times = [rtcTime,greenTime,redTime]
+displays = [white,green,red]
 
 while True:
     pressed = 0
-    times = [rtcTime,greenTime,redTime]
-    displays = [white,green,red]
-    
+
     if picounicorn.is_pressed(picounicorn.BUTTON_A):
         pressed = 1
         mode = (mode + 1) % 4
@@ -172,9 +180,10 @@ while True:
             hour = (hour - 1) % 24 if minute == 0 else hour
             minute = (minute - 1) % 60
             times[mode].datetime(hour,minute)
-         
+        # number for hour-tens, space, number for hours-ones, space, space, number for minute-tens, number for minute-ones
         string = [n[math.floor(hour/10)],s,n[hour%10],s,s,n[math.floor(minute/10)],s,n[minute%10]]
-          
+        
+        # xOff is the x-Offset - how far right / which columns we are drawing
         xOff = 0
         for item in string:
             for rowIdx, row in enumerate(item):
@@ -186,16 +195,20 @@ while True:
         linearNow = rtcTime.linear()
         pixel = red
         if(linearNow < greenTime.linear() or linearNow >= redTime.linear()):
+            #red light before wake (green) time, red light after sleep time
             pixel = red
         elif(linearNow - greenTime.linear() <= 60):
+            #green light for an hour after wake (green) time
             pixel = green
         else:
+            #don't confuse them; white if they are having a mid day nap
             pixel = white
-          
+        #every pixel  
         for y in range(7):
             for x in range(16):
                 picounicorn.set_pixel(x,y,pixel[0],pixel[1],pixel[2])
     
+    #presses make the response change faster (hold to skip many minutes)
     if pressed>0:
         utime.sleep(sleep)
         if pressed == 2:
